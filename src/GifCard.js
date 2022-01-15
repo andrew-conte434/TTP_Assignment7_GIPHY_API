9283b618fb69270aa6dab8793b2bd7a931484848
Andrew Conte <andrew-conte434>
2022-01-15 15:06:21 -0500

    added random button, trending GIFs, and .env file

--- src/GifCard.js
+++ src/GifCard.js
@@ -6,27 +6,31 @@ import { useEffect } from 'react';
 
 
 function GifCard(props){
-    const cards = new Array();
-    const gifs = props.data;
-    traverseData(gifs, cards, props.didUserEnterInfo);
-    return <h1>{cards}</h1>
+    return <img src={`https://i.giphy.com/media/${props.id}/giphy.gif`}/>
 }
 
-function traverseData(gifs, cards, isInputValid){
-    if(isInputValid && gifs != null){
-        const arr = gifs.data;
+// function GifCard(props){
+    
+//     const cards = new Array();
+//     const gifs = props.data;
+//     traverseData(gifs, cards, props.didUserEnterInfo);
+//     return <h1>{cards}</h1>
+// }
+
+// function traverseData(gifs, cards, isInputValid){
+//     if(isInputValid && gifs != null){
+//         const arr = gifs.data;
+//         for(let i = 0; i < arr.length; i++){
+//             const link = "https://i.giphy.com/media/" + arr[i].id + "/giphy.gif";       
+//             cards.push(<img src={link} key={"img0"+i} ></img>);
+//         }  
         
-        for(let i = 0; i < arr.length; i++){
-            const link = "https://i.giphy.com/media/" + arr[i].id + "/giphy.gif";       
-            cards.push(<img src={link} key={"img0"+i}></img>);
-        }  
-        
-        if(arr.length == 0){
-            cards.push("No Results Found");
-        }
-    } else {
-        cards = new Array();
-    }
-}
+//         if(arr.length == 0){
+//             cards.push("No Results Found");
+//         }
+//     } else {
+//         cards = new Array();
+//     }
+// }
 
 export default GifCard;
--- src/SearchField.js
+++ src/SearchField.js
@@ -7,6 +7,9 @@ import { useState } from 'react';
 function SearchField(props){
 
     const [input, setInput] = useState("");
+    const [gifs, setGifs] = useState(null)
+    let random = false
+    let error = false
 
     const updateInput = (e) => {
         if(e.key === 'Enter'){
@@ -15,10 +18,75 @@ function SearchField(props){
         }
     };
 
+    const handleEnter = (e) => {
+        random = false
+        if(e.key === 'Enter'){
+            e.preventDefault()
+            fetchData(e)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        random = false
+        e.preventDefault()
+        fetchData(e)
+    }
+
+    const handleRandom = (e) => {
+        e.preventDefault()
+        random = true
+        fetchData(e)
+    }
+
+    const fetchData = async (e) => {
+        const apiKey = process.env.REACT_APP_GIPHY_API_KEY
+        let path
+        path = random ? `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}` :
+                        `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=${apiKey}`
+        await fetch(path)
+        .then((res) => res.json())
+        .then((obj) => {
+            error = false
+            if(random){
+                //Creates array if API call returns singular object
+                let arr = []
+                arr.push(obj.data)
+                setGifs(arr)
+            } else {
+                setGifs(obj.data)
+            }
+        })
+        .catch((err) => {
+            error = true
+        })
+    }
+
     return (
-        <div className="search">
-            <label htmlFor={"gif-input"}> Search Up A Gif: </label>
-            <input type={"text"} name={"gif-input"} onKeyUp={(e) => updateInput(e)}/>
+        <div className="search-field">
+            <form>
+                <label htmlFor={"gif-input"}> Search Up A Gif: </label>
+                <input type={"text"} 
+                    name={"gif-input"} 
+                    onChange={(e) => setInput(e.target.value)}
+                    onKeyDown={handleEnter}/>
+                <input type={"button"} value={"Search"} onClick={handleSubmit}/>
+                <button className="random-button" onClick={handleRandom}>Get a random GIF!</button>
+            </form>
+
+
+            {/* {gifs && <h2>Searching up... {input}</h2>} */}
+            <div className='search-results'>
+                {error ? <GifCard key = {1}
+                                  id = {"UHAYP0FxJOmFBuOiC2"}/> : <></>}
+                {console.log(gifs)}
+                {gifs && gifs.map((gif, i) => {
+                    console.log(gif.id)
+                    return (
+                        <GifCard key = {i}
+                                id = {gif.id}/>
+                    )
+                })}
+            </div>
         </div>
     );
 }
